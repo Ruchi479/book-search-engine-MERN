3e8ca39572ed5e4ac1fa5f431abec810b61a1c1a
Ruchi <ruchisaini.0404@gmail.com>
2022-01-25 22:39:14 +1100

    updated password

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 console.log(process.env.MONGODB_URI);
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Ruchi:Ruchivsaini@47@cluster0.4wmbf.mongodb.net/Book-Search-App?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://Ruchi:Ruchivsaini%4047@cluster0.4wmbf.mongodb.net/Book-Search-App?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
